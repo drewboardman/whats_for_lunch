1a7d427a5c9ebc4dd8dd0d043c7a6f3bd3087875
Drew Boardman <drew.e.boardman@gmail.com>
2014-11-09 17:42:47 -0500

    Latest commit with logic half-way done.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,8 +1,18 @@
 class DashboardsController < ApplicationController
   before_filter :check_logged_in_user
 
+  def last_place_id
+    Visit.where(user_id: current_user.id).order('last_visit DESC').first.restaurant_id
+  end
+
+  def last_place_name(rest_id)
+    Restaurant.where(id: rest_id).name
+  end
+
   def show
     @restaurant = Restaurant.new
     @restaurants = Restaurant.all
+    @rest_id = self.last_place_id
+    @rest_name = last_place_name(4)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
   has_many :ratings
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
+
+  def last_place_id(id)
+    Visit.where(user_id: id).order("last_visit DESC").first.restaurant_id
+  end
+
 end
