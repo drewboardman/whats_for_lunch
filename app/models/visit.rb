4d2949d88b66031e87c757818ceec497f3a4028d
Drew Boardman <drew.e.boardman@gmail.com>
2014-11-10 14:20:05 -0500

    There is an issue with the date validation field and how last_visit? works. Originally you could send a mm/dd/yyyy value to the db. Now the default value is todays date, but there are gems for even stronger validation.

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,4 +1,6 @@
 class Visit < ActiveRecord::Base
   belongs_to :user
   belongs_to :restaurant
+
+  # validates :last_visit, :presence => true, :format => /(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d/
 end
